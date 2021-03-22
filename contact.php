6f61070f1b648464074ddf6efa24f773f6c68cea
vaibhav-Tayal <karan@vridhisoftech.com>
2021-03-22 15:52:00 +0530

    Contact PAge

---
+++ contact.php
@@ -0,0 +1,177 @@
+<?php include_once( "header.php") ?>
+<section class="inner-banner-sec" style="background-image: url('images/inner-page-banner.jpg');">
+  <div class="vertical-middle">
+    <div class="container">
+      <div class="banner-text">
+        <div class="row align-items-center">
+          <div class="col-lg-9">
+            <div class="inner-banner-title">
+              <h1>Contact Us</h1>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="breadcrumb">
+              <span>
+                <a href="#">
+                  <span><i class="fa fa-home" aria-hidden="true"></i> Home</span>
+                </a>
+              </span>><span>Contact Us</span>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<section class="contact-detail pb-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-6" >
+                <a href="tel:8456788788" class="cd-hover">
+                    <div class="contact-detail-item grey-bg">
+                        <div class="cdi-bg-icon">
+                            <img src="images/phone-icon-black.png">
+                        </div>
+                        <div class="cdi-inner">
+                            <div class="detail-item-icon">
+                                <div class="dii-inner">
+                                    <img src="images/phone-icon-black.png">
+                                </div>
+                            </div>
+                            <div class="detail-item-text" style="visibility: visible; ">
+                                <h5>Phone</h5>
+                                <p>999-999-9999</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-6">
+                <a href="mailto:cojaproject@gmail.com" class="cd-hover">
+                    <div class="contact-detail-item grey-bg">
+                        <div class="cdi-bg-icon">
+                            <img src="images/email-icon-black.png">
+                        </div>
+                        <div class="cdi-inner">
+                            <div class="detail-item-icon">
+                                <div class="dii-inner">
+                                    <img src="images/email-icon-black.png">
+                                </div>
+                            </div>
+                            <div class="detail-item-text" style="visibility: visible; ">
+                                <h5>Email</h5>
+                                <p>cojaproject@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-6" >
+                <a href="https://www.google.co.in/maps/place/New+York,+NY,+USA/@40.6976701,-74.2598682,10z/data=!3m1!4b1!4m5!3m4!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728?hl=en" class="cd-hover" target="_blank">
+                    <div class="contact-detail-item grey-bg">
+                        <div class="cdi-bg-icon">
+                            <img src="images/address-icon.png">
+                        </div>
+                        <div class="cdi-inner">
+                            <div class="detail-item-icon">
+                                <div class="dii-inner">
+                                    <img src="images/address-icon.png">
+                                </div>
+                            </div>
+                            <div class="detail-item-text" style="visibility: visible; ">
+                                <h5>Address</h5>
+                                <p>Coja Projects, NY, USA</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /section end -->
+<section class="contact-form-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-8 col-xs-12 cf-left-side">
+                <div class="title-left">
+                    <h2>send us <span>your queries</span></h2>
+                    <p >Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+                </div>
+                <form class="form-horizontal row form-style">
+                  <div class="form-group col-lg-6">
+                    <input name="textinput" type="text" placeholder="Name" class="form-control input-md">
+                  </div>
+
+                  <!-- Text input-->
+                  <div class="form-group col-lg-6">
+                    <input name="textinput" type="text" placeholder="Email" class="form-control input-md">
+                  </div>
+
+                  <!-- Textarea -->
+                  <div class="form-group col-lg-12">
+                    <textarea class="form-control" id="textarea" name="textarea" placeholder="Message"></textarea>
+                  </div>
+
+                  <!-- Button -->
+                  <div class="form-group col-md-4">
+                      <button id="singlebutton" name="singlebutton" class="btn btn-secondary">Submit</button>
+                  </div>
+                  </form>
+
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12 cf-right-side">
+                <img src="images/form-person.png">  
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Section End -->
+<section class="contact-map p-0">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.2798911863!2d-74.25986818535776!3d40.69767006766623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1611050294271!5m2!1sen!2sin" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</section>
+<!-- Section End -->
+<section class="call-to-action-sec" style="background-image: url('images/call-to-action-bg.jpg');">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>We Are Ready To Work With You</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="#" class="btn btn-primary">Get A Quote</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<section class="client-logo-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="title">
+          <h2>Our <span>Partner</span></h2>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <ul class="client-logo-item">
+          <li><img src="images/icon1.png"></li>
+          <li><img src="images/icon2.png"></li>
+          <li><img src="images/icon3.png"></li>
+          <li><img src="images/icon4.png"></li>
+          <li><img src="images/icon5.png"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<style type="text/css">
+  .call-to-action-sec {
+    margin-top: -10px;
+}
+</style>
+<?php include_once("footer.php"); ?>
